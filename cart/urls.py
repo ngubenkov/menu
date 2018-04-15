b616ecfac1a15b0e4cad41a857e07683d573acef
nick <frozmanik@gmail.com>
2018-04-15 10:38:11 +0300

    url subbmit

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.CartDetail, name='CartDetail'),
     url(r'^remove/(?P<product_id>\d+)/$', views.CartRemove, name='CartRemove'),
-    url(r'^add/(?P<product_id>\d+)/$', views.CartAdd, name='CartAdd')
+    url(r'^add/(?P<product_id>\d+)/$', views.CartAdd, name='CartAdd'),
+    url(r'^/cart/submit/$', views.CartSubmit, name='CartSubmit')
+
 ]
